d2c4696e9ab89dd232221dfa1a69d2401c74a083
Jerry Krinock <jerry@sheepsystems.com>
2020-07-26 14:43:19 -0700

    Demonstrates how NSTabViewController does not populate its tab view with its tab view items :(

--- NSTabViewControllerDemo/AppDelegate.swift
+++ NSTabViewControllerDemo/AppDelegate.swift
@@ -1,26 +1,42 @@
-//
-//  AppDelegate.swift
-//  NSTabViewControllerDemo
-//
-//  Created by Jerry on 20-07-24.
-//
-
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet var window: NSWindow!
-
+    @IBOutlet var tabView: NSTabView!
+    @IBOutlet var view1: NSView!
+    @IBOutlet var view2: NSView!
+    var tabViewController: NSTabViewController? = nil
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        self.tabViewController = NSTabViewController()
+        if let tabViewController = self.tabViewController {
+            tabViewController.tabView = self.tabView;
+        }
+        
+        self.addTabViewItem(view: view1,
+                            label: "Label Uno",
+                            identifier: "Identifier Uno")
+        self.addTabViewItem(view: view2,
+                            label: "Label Dos",
+                            identifier: "Identifier Dos")
+        
+        print("tab view controller's tab view items: \(String(describing: self.tabViewController?.tabViewItems))")
+        print("tab view's tab view items: \(String(describing: self.tabView?.tabViewItems))")
+        
+        self.tabView .display()
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    
+    func addTabViewItem(view: NSView, label: String!, identifier: String!) {
+        let childViewController = NSViewController();
+        childViewController.view = view;
+        self.tabViewController?.addChild(childViewController)
+        if let tabViewItem = self.tabViewController?.tabViewItem(for: childViewController) {
+            tabViewItem.identifier = identifier
+            tabViewItem.label = label;
+            tabViewItem.view = childViewController.view;
+        }
     }
-
-
 }
 
